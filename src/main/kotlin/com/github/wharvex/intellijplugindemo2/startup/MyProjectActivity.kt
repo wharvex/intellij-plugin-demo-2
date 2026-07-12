1db4b5a6629916d476566cdda912bc112d4ff516
Timothy Gudlewski <t.gudlewski@gmail.com>
2026-07-12 14:22:07 -0400

    updates

--- src/main/kotlin/com/github/wharvex/intellijplugindemo2/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/wharvex/intellijplugindemo2/startup/MyProjectActivity.kt
@@ -1,11 +1,13 @@
 package com.github.wharvex.intellijplugindemo2.startup
 
+import com.intellij.database.console.JdbcDriverManager
 import com.intellij.database.console.session.DatabaseSessionManager
 import com.intellij.database.dataSource.DatabaseConnectionManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.database.console.session.canClose
 import com.intellij.database.console.session.close
+import com.intellij.database.dataSource.LocalDataSource
 
 class MyProjectActivity : ProjectActivity {
 
@@ -16,9 +18,22 @@ class MyProjectActivity : ProjectActivity {
                 if (added) {
                     // triggered when a new DB connection is created
                     val newDataSource = connection.connectionPoint.dataSource
+                    val oldDataSources = mutableSetOf<LocalDataSource>()
                     for (session in DatabaseSessionManager.getSessions(project)) {
-                        if (session.connectionPoint.dataSource !== newDataSource && canClose(session)) {
+                        val dataSource = session.connectionPoint.dataSource
+                        if (dataSource !== newDataSource && canClose(session)) {
                             close(session)
+                            oldDataSources.add(dataSource)
+                        }
+                    }
+
+                    // close(session) only tears down the console session UI object; it never
+                    // releases the underlying JDBC driver process, so isConnected() would keep
+                    // reporting these data sources as connected without this.
+                    val driverManager = JdbcDriverManager.getDriverManager(project)
+                    for (dataSource in oldDataSources) {
+                        for (configuration in driverManager.getActiveConfigurations(dataSource)) {
+                            driverManager.releaseDriver(dataSource, configuration)
                         }
                     }
                 }
